8bf8006c6404767df24d8f136b27b19c42a3e1bf
Andrew Walker <andrew.walker@truenas.com>
2025-12-04 09:11:19 -0600

    Fix otpw authentication
    This commit fixes a situation in which the pam context was
    prematurely closed in cases where we need to keep it around
    for further PAM calls (authentication may have components
    outside of PAM module).

--- src/truenas_authenticator/authenticator.py
+++ src/truenas_authenticator/authenticator.py
@@ -112,11 +112,8 @@ def _auth_thread_worker(thread_state: ConversationThreadState):
             for key, value in thread_state.pam_env.items():
                 ctx.set_env(name=key, value=value)
 
-        # Perform authentication
-        ctx.authenticate()
-
-        # Store context on success
         thread_state.pam_context = ctx
+        ctx.authenticate()
 
     except Exception as e:
         thread_state.exception = e
@@ -243,7 +240,13 @@ class UserPamAuthenticator:
                 code = truenas_pypam.PAMCode.PAM_SYSTEM_ERR
                 reason = str(self._thread_state.exception)
 
-            self.end()
+            if self.state.otpw_possible:
+                # When this flag is set we want to keep the PAM context around
+                # until cleanup or explicit consumer call of self.end()
+                self.ctx = self._thread_state.pam_context
+            else:
+                self.end()
+
             return AuthenticatorResponse(AuthenticatorStage.AUTH, code, reason)
 
         self.ctx = self._thread_state.pam_context
